5359b0d3930de1fc85c49c18fd6f67bc1560afed
michaels90up <michael1990spw@gmail.com>
2023-03-08 10:42:16 +0100

    Components and props intro

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import Post from './components/Post';
+
+function App() {
+  return (
+    <main>
+      <Post author="Michael" city="Vienna" />
+      <Post author="Tim" city="Berlin" />
+    </main>
+  );
+}
+
+export default App;
---
+++ src/components/Post.js
@@ -0,0 +1,10 @@
+function Post(props) {
+  return (
+    <div>
+      <p>{props.author}</p>
+      <p>{props.city}</p>
+    </div>
+  );
+}
+
+export default Post;
